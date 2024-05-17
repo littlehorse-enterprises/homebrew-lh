3810a8dc268a4d3526dbbb86787c15c73cff96be
Colt McNealy <colt@littlehorse.io>
2024-05-16 22:28:51 -0700

    Updates to 0.9.0 and pins 0.8

--- Formula/lhctl@0.8.rb
+++ Formula/lhctl@0.8.rb
@@ -2,8 +2,8 @@ class Lhctl < Formula
   desc "LittleHorse CLI for managing all things LittleHorse"
   homepage "https://littlehorse.dev/docs/developer-guide/lhctl"
   url "https://github.com/littlehorse-enterprises/littlehorse.git",
-      tag:      "0.9.0",
-      revision: "050e379f3cdb07d43902a1e9ae9b4dc474af5a65"
+      tag:      "0.8.1",
+      revision: "d5cdffe39e5edb9a55958aa1cf2254ae33dd921f"
   license "SSPL-1.0"
   head "https://github.com/littlehorse-enterprises/littlehorse.git", branch: "master"
 
