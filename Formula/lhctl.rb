9ea823334e3a687de9b4d365901e3f724c63027b
Colt McNealy <colt@littlehorse.io>
2024-06-23 23:08:45 -0700

    Updates to 0.10.0

--- Formula/lhctl.rb
+++ Formula/lhctl.rb
@@ -2,8 +2,8 @@ class Lhctl < Formula
   desc "LittleHorse CLI for managing all things LittleHorse"
   homepage "https://littlehorse.dev/docs/developer-guide/lhctl"
   url "https://github.com/littlehorse-enterprises/littlehorse.git",
-      tag:      "0.9.0",
-      revision: "050e379f3cdb07d43902a1e9ae9b4dc474af5a65"
+      tag:      "0.10.0",
+      revision: "e16a552cc8761058a65f9f69298bea3f360716e8"
   license "SSPL-1.0"
   head "https://github.com/littlehorse-enterprises/littlehorse.git", branch: "master"
 
