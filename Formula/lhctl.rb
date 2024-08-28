631a0978b32a82902086433f77d55c4dce78d487
Mijail Rondon <mijail@riseup.net>
2024-08-28 14:49:21 -0500

    build(lhctl): upgrade 0.11.1

--- Formula/lhctl.rb
+++ Formula/lhctl.rb
@@ -2,15 +2,14 @@ class Lhctl < Formula
   desc "LittleHorse CLI for managing all things LittleHorse"
   homepage "https://littlehorse.dev/docs/developer-guide/lhctl"
   url "https://github.com/littlehorse-enterprises/littlehorse.git",
-      tag:      "v0.11.0",
-      revision: "4b018c1b62e6104ddff1e62f7631ed88b3393352"
+      tag:      "v0.11.1",
+      revision: "81588bd6a497177b130a98e115dff7fc3b5ba6e0"
   license "SSPL-1.0"
-  head "https://github.com/littlehorse-enterprises/littlehorse.git", tag: "v0.11.0"
+  head "https://github.com/littlehorse-enterprises/littlehorse.git", tag: "v0.11.1"
 
   depends_on "go" => :build
 
   def install
-    system "./local-dev/setup-go.sh"
     ldflags = "-X main.version=#{version} -X main.commit=homebrew"
     system "go", "build", "-C", "lhctl", *std_go_args(ldflags:)
   end
