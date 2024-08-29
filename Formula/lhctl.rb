3ec59659e057779dee1f89fdc1cfe1ecb93cf6f5
Mijail Rondon <mijail@riseup.net>
2024-08-29 09:11:34 -0500

    build(lhctl): upgrade 0.11.2

--- Formula/lhctl.rb
+++ Formula/lhctl.rb
@@ -2,10 +2,10 @@ class Lhctl < Formula
   desc "LittleHorse CLI for managing all things LittleHorse"
   homepage "https://littlehorse.dev/docs/developer-guide/lhctl"
   url "https://github.com/littlehorse-enterprises/littlehorse.git",
-      tag:      "v0.11.1",
-      revision: "81588bd6a497177b130a98e115dff7fc3b5ba6e0"
+      tag:      "v0.11.2",
+      revision: "fd1ff8340336b411a0f208b9bd4d01fed0d5985f"
   license "SSPL-1.0"
-  head "https://github.com/littlehorse-enterprises/littlehorse.git", tag: "v0.11.1"
+  head "https://github.com/littlehorse-enterprises/littlehorse.git", tag: "v0.11.2"
 
   depends_on "go" => :build
 
