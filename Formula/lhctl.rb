31f07a186d2d16aa14601b752557e78bf88f6510
Saúl Piña <saul@littlehorse.io>
2025-01-28 11:50:22 -0500

    build(homebrew): update url position

--- Formula/lhctl.rb
+++ Formula/lhctl.rb
@@ -1,8 +1,8 @@
 class Lhctl < Formula
   desc "LittleHorse CLI for managing all things LittleHorse"
   homepage "https://littlehorse.io/docs/server/developer-guide/lhctl"
-  license "SSPL-1.0"
   url "https://github.com/littlehorse-enterprises/littlehorse.git", tag: "v0.12.4"
+  license "SSPL-1.0"
   head "https://github.com/littlehorse-enterprises/littlehorse.git", tag: "v0.12.4"
 
   depends_on "go" => :build
