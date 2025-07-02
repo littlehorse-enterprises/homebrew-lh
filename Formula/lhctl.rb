8403179510852f4399a7591fecedcb45941aa556
Jacob Snarr <jacobsnarr@gmail.com>
2025-07-02 18:53:51 -0400

    build(lhctl): Upgrade to v0.14.1 (#15)

--- Formula/lhctl.rb
+++ Formula/lhctl.rb
@@ -1,9 +1,9 @@
 class Lhctl < Formula
   desc "LittleHorse CLI for managing all things LittleHorse"
   homepage "https://littlehorse.io/docs/server/developer-guide/lhctl"
-  url "https://github.com/littlehorse-enterprises/littlehorse.git", tag: "v0.14.0"
+  url "https://github.com/littlehorse-enterprises/littlehorse.git", tag: "v0.14.1"
   license "SSPL-1.0"
-  head "https://github.com/littlehorse-enterprises/littlehorse.git", tag: "v0.14.0"
+  head "https://github.com/littlehorse-enterprises/littlehorse.git", tag: "v0.14.1"
 
   depends_on "go" => :build
 
